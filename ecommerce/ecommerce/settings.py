6d9a0372efdd23dd159c4ce95bf736d18b8e1753
Ian Chebii <ianchebii19@gmail.com>
2024-06-05 12:07:30 +0300

    no vercel

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-19_-gz5a3%33e4mlftt1k8^!c9&-=rp^%u)@2m0&8(jvksjk52'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
